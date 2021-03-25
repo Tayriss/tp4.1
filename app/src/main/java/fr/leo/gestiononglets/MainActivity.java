ab50b467f7001f4a520ee62b9a6790ce72f5a578
Tayriss <lelerougeole@hotmail.fr>
2021-03-25 14:27:50 +0100

    Initial commit

---
+++ app/src/main/java/fr/leo/gestiononglets/MainActivity.java
@@ -0,0 +1,62 @@
+package fr.leo.gestiononglets;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+import fr.leo.gestiononglets.R;
+import fr.leo.gestiononglets.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        if(id==R.id.item1){
+            Snackbar.make(viewPager, "Item1", BaseTransientBottomBar.LENGTH_SHORT).show();
+            return true;
+        }
+
+        if(id==R.id.item2){
+            Snackbar.make(viewPager, "Item2", BaseTransientBottomBar.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
